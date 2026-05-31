92e0ca9016a8817e7fdee459291d877394d00c7d
Leon <xl033379@gmail.com>
2026-05-31 21:47:57 +0800

    feat(group-ui): groups feature types and api client

---
+++ web/default/src/features/groups/types.ts
@@ -0,0 +1,56 @@
+/*
+Copyright (C) 2023-2026 QuantumNous
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+For commercial licensing, please contact support@quantumnous.com
+*/
+export type Group = {
+  id: number
+  name: string
+  description: string
+  consumption_ratio: number
+  topup_ratio: number
+  visibility: 'public' | 'private'
+  admin_only: boolean
+  auto_upgrade: boolean
+  upgrade_threshold: number
+  in_auto_rotation: boolean
+  auto_order: number
+  created_time: number
+  updated_time: number
+}
+
+export type GroupManageItem = Group & { channel_count: number }
+
+export type GroupManageListData = {
+  groups: GroupManageItem[]
+  default_channel_group: string
+  new_user_default_group: string
+  default_use_auto_group: boolean
+}
+
+export type GroupChannel = {
+  id: number
+  name: string
+  status: number
+  group: string
+  type: number
+}
+
+export type ApiResponse<T> = {
+  success: boolean
+  message?: string
+  data: T
+}
